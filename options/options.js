1e78632bf0bcafa88eaac1a71b2f5c40ba108540
Crowdbotics <team@crowdbotics.com>
2021-10-22 06:54:55 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "holy_butterfly_31638",
+  url: "https://holy_butterfly_31638.botics.co",
+  api: "https://holy_butterfly_31638.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
